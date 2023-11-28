0748335223c2229f76d3f84c548734812439d3a2
Hakan Bingöl <hakan.bingol@outlook.com>
2023-11-29 00:02:55 +0300

    ASP.NET APPUSER VE APPROLE TABLOLARI OLUŞTURULDU

--- EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -10,11 +10,12 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Concrete
 {
-    public class Context : IdentityDbContext    
+    public class Context : IdentityDbContext<AppUser, AppRole, int>    
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=DESKTOP-AF0RTPO;inital catalog=EasyCashDb; integrated security=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-AF0RTPO;initial catalog=EasyCashDb; integrated security=true");
+            //optionsBuilder.UseSqlServer("server=77.245.159.8\\EasyCashIdentityProject;initial catalog=EasyCashDb; userid=HakanBingol; Password=Hakan1920x!; integrated security=true");
         }
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
